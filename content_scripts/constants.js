6b706a7dedd653325e171e7cd1a671a3f8cf508d
prefabcode <prefabcode@gmail.com>
2024-07-17 07:58:48 -0400

    begin work on perks system

---
+++ content_scripts/constants.js
@@ -0,0 +1,6 @@
+export const levelNames = [
+    "Brown", "Wood", "Wood2", "Wood3", "Wood4", "Maple", "Maple2", "Horsey", "Leather", "Blue",
+    "Blue2", "Blue3", "Canvas", "Blue-Marble", "IC", "Green", "Marble", "Green-Plastic", "Olive", "Grey",
+    "Metal", "Newspaper", "Purple", "Purple-Diag", "Pink"
+];
+
--- content_scripts/uiUpdates.js
+++ content_scripts/uiUpdates.js
@@ -1,10 +1,5 @@
 import { exportExtensionState, importExtensionState, confirmResetProgress } from './storageManagement.js';
-
-export const levelNames = [
-    "Brown", "Wood", "Wood2", "Wood3", "Wood4", "Maple", "Maple2", "Horsey", "Leather", "Blue",
-    "Blue2", "Blue3", "Canvas", "Blue-Marble", "IC", "Green", "Marble", "Green-Plastic", "Olive", "Grey",
-    "Metal", "Newspaper", "Purple", "Purple-Diag", "Pink"
-];
+import { levelNames } from './constants.js';
 
 export const updateProgressBar = (completedBoards = null, hueValue = null) => {
     chrome.storage.local.get(['completedBoards'], (result) => {
